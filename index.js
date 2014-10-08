64d24c9333d7dc91e5207b4aafc12f391a90fd68
D. Darren MacDonald <drrnmcdnld@gmail.com>
2014-10-08 12:56:04 -0300

    Update after working server

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
+app.use("/style.css", express.static(__dirname + '/style.css'));
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
